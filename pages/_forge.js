724b49cf4f77901863ea0ad53e5f4f528cb5403e
pcdestroid <alanfrancisangra@gmail.com>
2023-02-25 00:17:38 -0300

    Add forge

--- pages/_forge.js
+++ pages/_forge.js
@@ -19,8 +19,6 @@ const Forge = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@700&family=Space+Mono&display=swap" rel="stylesheet" />
-        <script src="./fire.js"></script>
-
       </Head>
 
       <div class="main">
